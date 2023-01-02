77cf633320e0defe31d9032fad7fb99df4d82c1c
Mrvikki90t <montysn2@gmail.com>
2023-01-02 18:53:25 +0530

    comit 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,15 +5,13 @@ import JoinForm from "./components/JoinForm";
 
 function App() {
   return (
-    <Box h="100vh">
-      <Router>
-        <Routes>
-          <Route path="/" element={<JoinForm />}></Route>
-          <Route path="/chat/:roomname" element={<ChatRoom />}></Route>
-          <Route path="*" element={<h1>404 Not Found</h1>}></Route>
-        </Routes>
-      </Router>
-    </Box>
+    <Router>
+      <Routes>
+        <Route path="/" element={<JoinForm />}></Route>
+        <Route path="/chat/:roomname" element={<ChatRoom />}></Route>
+        <Route path="*" element={<h1>404 Not Found</h1>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
